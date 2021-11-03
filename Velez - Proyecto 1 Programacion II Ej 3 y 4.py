e9343aeeed9399a4dfa9f7424e3bbdfdcc6b4bbd
Facundo Vélez <facuvelez350@gmail.com>
2021-11-02 22:57:17 -0300

    Respaldo e inicio ej 3
    Respaldo de ejercicios anteriores, inicio ejercicio 3

---
+++ Velez - Proyecto 1 Programacion II Ej 3 y 4.py
@@ -0,0 +1,34 @@
+#Ejercicio 1: Expresiones regulares
+
+#Matrículas válidas:
+
+#LV-QWE
+#LQ-ABE
+#LV-X443
+#LV-S586
+#LV-SX334
+
+#Matrículas inválidas:
+
+#LA-123
+#LX-ABC
+#LV
+#LV-344
+
+#Respuesta: ^L\w\W?\w?\w?\w?\w?\w?
+
+#Ejercicio 2: Expresiones regulares
+
+#1901
+#1900
+#1850
+#500
+#12
+#2
+
+#Respuesta: ^((1900)|(1[0-8][0-9]{2})|([1-9][0-9]{2})|([1-9][0-9]{1})|([1-9]))$
+
+#Ejercicio 3: Recursión
+
+
+
